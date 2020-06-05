b7336cb856bb241efec0b79bc00fb4d2461ff8cd
Gustavo Martins <gustavosm994@gmail.com>
2020-06-05 16:09:44 -0300

    api IBGE

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -26,7 +26,8 @@ const CreatePoint = () => {
   const [items, setItems] = useState<Item[]>([]);
   const [ufs, setUfs] = useState<string[]>([]);
   const [cities, setCities] = useState<string[]>([]);
-  
+  const [selectedCity, setSelectedCity] = useState('0');
+
   const [selectedUf, setSelectedUf] = useState('0');
 
   useEffect(() => {
@@ -58,6 +59,11 @@ const CreatePoint = () => {
   function handleSelectUf(event: ChangeEvent<HTMLSelectElement>) {
     const uf = event.target.value;
     setSelectedUf(uf);
+  }
+
+  function handleSelectCity(event: ChangeEvent<HTMLSelectElement>) {
+    const city = event.target.value;
+    setSelectedCity(city);
   }
 
   return (
@@ -138,7 +144,7 @@ const CreatePoint = () => {
             </div>
             <div className="field">
               <label htmlFor="city">Cidade</label>
-              <select name="city" id="city">
+              <select name="city" id="city" value={selectedCity} onChange={handleSelectCity}>
                 <option value="0">Selecione uma cidade</option>
                 {cities.map(city => (
                   <option key={city} value={city}>{city}</option>
